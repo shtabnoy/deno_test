34c54ea94baf2e6a7d3d6d0add98641c269b977f
Denis Shtabnoy <denis.shtabnoy@gmail.com>
2020-02-27 22:59:38 +0100

    rename index.html to intro.html

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ const startServer = async () => {
   const port = 8000
   const s = serve({ port: 8000 })
   const decoder = new TextDecoder('utf-8')
-  let html = await Deno.readFile('./index.html')
+  let html = await Deno.readFile('./intro.html')
   let tosend = decoder.decode(html)
   console.log('Server started on port ', port)
   for await (const req of s) {
